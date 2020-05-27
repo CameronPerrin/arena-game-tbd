4fea7e3b109cc578871700ba8b56b96c0740422c
josh <joshsoran@gmail.com>
2020-05-26 20:05:27 -0400

    Player+Camera+PLane
    Co-Authored-By: Cameron Perrin <perrin.cameron@gmail.com>

---
+++ Arena TBD/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject mainCam;
+    public float xPosition, yPosition, zPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraMove();
+    }
+
+    private void cameraMove()
+    {
+        mainCam.transform.position = this.transform.position + new Vector3(xPosition, yPosition, -zPosition);
+        mainCam.transform.LookAt(this.transform.position);
+    }
+}
---
+++ Arena TBD/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+    }
+}
