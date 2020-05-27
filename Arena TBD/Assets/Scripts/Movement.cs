d69b6076616bab52bc56332f768030c9e289ef00
josh <joshsoran@gmail.com>
2020-05-26 22:31:00 -0400

    Fixed Movement Rotation, added design to our player, improved movement
    Co-Authored-By: Cameron Perrin <perrin.cameron@gmail.com>

--- Arena TBD/Assets/Scripts/Movement.cs
+++ Arena TBD/Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
 
 
     // Start is called before the first frame update
@@ -18,19 +20,33 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey("d"))
         {
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
         }
         if (Input.GetKey("a"))
         {
-            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0, Space.World);
         }
         if (Input.GetKey("w"))
         {
-            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("s"))
         {
-            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime, Space.World);
+        }
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); ;
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            Vector3 target = hit.point;
+            target.y = 0;
+            target.y = transform.localScale.y / 2f;
+
+            transform.LookAt(new Vector3(target.x, transform.position.y, target.z));
         }
     }
 }
