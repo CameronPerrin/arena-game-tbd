c219ac9155bc0cf1c2077f9ab5a2ed5bd27bd2d2
Cameron Perrin <perrin.cameron@gmail.com>
2020-05-27 12:42:50 -0400

    Added player health and death
    Co-Authored-By: trans12 <trans12@users.noreply.github.com>

---
+++ Arena TBD/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealthController : MonoBehaviour
+{
+	public int health = 100;
+	public TMP_Text healthBox;
+	public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		healthBox.text = health.ToString();
+		if(Input.GetMouseButtonDown(0)){
+			health = health - 5; 
+		}
+		if(health <= 0){
+			Destroy(player);
+		}
+    }
+
+    void TakeDamage()
+    {
+
+    }
+}
