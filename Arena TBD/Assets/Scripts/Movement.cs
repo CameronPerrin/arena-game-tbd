4d4bacc12b8351ac6fec890a7679c8ac9a596db4
Cameron Perrin <perrin.cameron@gmail.com>
2020-05-28 13:57:15 -0400

    added jump and tweaked physics

--- Arena TBD/Assets/Scripts/Movement.cs
+++ Arena TBD/Assets/Scripts/Movement.cs
@@ -6,15 +6,27 @@ using UnityEngine.Networking;
 public class Movement : NetworkBehaviour
 {
     public float moveSpeed = 5f;
-    public float horizontalSpeed = 2.0F;
-    public float verticalSpeed = 2.0F;
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+    public float jumpHeight = 2.0f;
+    
+    [HideInInspector]
+    public Vector3 jump;
+    [HideInInspector]
+    public bool isGrounded;
+    [HideInInspector]
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+		rb = GetComponent<Rigidbody>();
+		jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
+	void OnCollisionStay(){
+        isGrounded = true;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -32,6 +44,16 @@ public class Movement : NetworkBehaviour
             transform.Translate(0, 0, moveSpeed * Time.deltaTime, Space.World);
         if (Input.GetKey("s"))
             transform.Translate(0, 0, -moveSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey("space") && isGrounded){
+        	// rb.AddForce(Vector3.up * 4.0f);
+        	rb.AddForce(jump * jumpHeight, ForceMode.Impulse);
+        	isGrounded = false;
+        }
+        if(!isGrounded && rb.velocity.y < 0f) // still jumping and falling
+     	{
+         	rb.AddForce(Vector3.down * 4.0f);
+     	}
 
         // Camera Look
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); ;
