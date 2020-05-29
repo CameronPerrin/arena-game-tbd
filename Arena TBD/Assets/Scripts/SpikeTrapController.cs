58c459312df985a41e3384a3159d5d448130e6f3
Cameron Perrin <perrin.cameron@gmail.com>
2020-05-29 18:44:41 -0400

    Make the spike trap visual with stor assets and added knock up to them

--- Arena TBD/Assets/Scripts/SpikeTrapController.cs
+++ Arena TBD/Assets/Scripts/SpikeTrapController.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class SpikeTrapController : MonoBehaviour
 {
-	Material m_Material;
+	Animation spikeAnim;
 	[HideInInspector]
 	GameObject player;
     // Start is called before the first frame update
     void Start()
     {
 		//Fetch the Material from the Renderer of the GameObject
-    	m_Material = GetComponent<Renderer>().material;
+    	spikeAnim = GetComponent<Animation>();
+        //spikeAnim.speed = 5;
         
     }
 
@@ -28,16 +29,17 @@ public class SpikeTrapController : MonoBehaviour
         {
         	player = collision.gameObject;
         	player.GetComponent<PlayerHealthController>().TakeDamage(20);
+            player.GetComponent<Rigidbody>().AddForce(transform.up * 1000f);
             //Debug.Log("TRAPPED!");
 
             //switch to spike up version
-            m_Material.color = Color.red;
+            spikeAnim.Play("Anim_TrapNeedle_Show");
         }
     }
     void OnCollisionExit(Collision collision)
     {
     	//switch to spike down version
-    	m_Material.color = Color.blue;
+    	spikeAnim.Play("Anim_TrapNeedle_Hide");
     }
 
 }
