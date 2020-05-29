9fc699a1df8773d71a225b26dd7a02ced988a727
josh <joshsoran@gmail.com>
2020-05-29 13:53:22 -0400

    Added Player Names over network

--- Arena TBD/Assets/Scripts/PlayerNameHandler.cs
+++ Arena TBD/Assets/Scripts/PlayerNameHandler.cs
@@ -6,35 +6,35 @@ using UnityEngine.UI;
 
 public class PlayerNameHandler : NetworkBehaviour
 {
-    // public GameObject nameController = GameObject.Find("NameManager");
-    [SyncVar (hook = "ChangeName")]
+    public GameObject nameController;
+    public GameObject nameText;
+    public string storedName;
+
+    [SyncVar]
     public string playerName;
 
-    void OnGUI()
-    {
-        if (!isLocalPlayer)
-        {
-            playerName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), playerName);
-            if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "Change"))
-            {
-                GetComponent<TextMesh>().text = playerName;
-            }
-        }
-    }
 
-  
-    public void ChangeName(string newName)
+
+    [Command]
+    public void CmdChangeName(string newName)
     {
         Debug.Log("Entered Name Change function");
         playerName = newName;
     }
 
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            nameController = GameObject.Find("NameManager");
+            storedName = nameController.GetComponent<StartGame>().passedName;
+            CmdChangeName(storedName);
+        }
+    }
+
     void Update()
     {
-        if(isLocalPlayer)
-        {
-            GetComponent<TextMesh>().text = playerName;
-        }
+        nameText.GetComponent<TextMesh>().text = playerName;
     }
 
 }
