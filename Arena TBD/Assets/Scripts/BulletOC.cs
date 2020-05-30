53711e06163a33d7c35b92e224095e8008b68de0
Cameron Perrin <perrin.cameron@gmail.com>
2020-05-30 15:56:20 -0400

    Added rotation and expanshion and shrinkage to the fireball

--- Arena TBD/Assets/Scripts/BulletOC.cs
+++ Arena TBD/Assets/Scripts/BulletOC.cs
@@ -10,6 +10,8 @@ public class BulletOC : NetworkBehaviour
     public int dmg = 5;
     public float tiltAroundX;
     public float tiltAroundZ;
+    public bool doExpand = false; 
+    public bool doShrink = false; 
     void OnTriggerEnter(Collider collision)
     {
         GameObject hit = collision.gameObject;
@@ -22,17 +24,33 @@ public class BulletOC : NetworkBehaviour
                 //hit.GetComponent<Rigidbody>().AddForce(transform.forward * 500f);
                 hit.GetComponent<Rigidbody>().AddForce(transform.up * 750f);
         }
-        gameObject.transform.localScale = new Vector3(10f, 10f, 10f);
+        //gameObject.transform.localScale = new Vector3(8f, 8f, 8f);
         gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
         gameObject.GetComponent<Rigidbody>().useGravity = false;
+        doExpand = true;
+
         //Destroy(gameObject);
     }
 
     void Update(){
         tiltAroundX += 2;
-        tiltAroundZ += 2;
+        tiltAroundZ += 8;
         //rotationVector.z += 0;
         Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
         gameObject.transform.rotation = target;
+        expandDong();
+    }
+
+    void expandDong(){
+        if(doExpand){
+            gameObject.transform.localScale += new Vector3(0.3f, 0.3f, 0.3f);
+        }
+        if(gameObject.transform.localScale.x > 10f){
+            doExpand = false;
+            doShrink = true;
+        }
+        if(doShrink){
+            gameObject.transform.localScale -= new Vector3(0.03f, 0.03f, 0.03f);
+        }
     }
 }
